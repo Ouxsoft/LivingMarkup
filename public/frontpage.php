56397ca7546b24291f63487ecb930e01398e66c3
hxtree <matthewheroux@gmail.com>
2020-05-15 07:48:13 -0400

    Moved apache2 config into own folder added hero image scss added custom scss include remove bootstrap duplicate from public added offset support to image crop api renamed home frontpage for clarity removed imageresize junk file

--- public/frontpage.php
+++ public/frontpage.php
@@ -8,12 +8,13 @@
     <body>
         <partial name="HeaderDefault"/>
 
-        <header class="masthead" style="background-image:url('assets/images/dimension/1400x400/offset/0,-50/livingmarkup/hero/oscar-fish.jpg'); height: 600px; background-size: cover; no-repeat center center fixed; width: 100%">
+        <header class="masthead hero-image" style="background-image:url('assets/images/dimension/1440x400/offset/0,-20/livingmarkup/hero/oscar-fish.jpg');">
             <div class="container h-100">
                 <div class="row h-100 align-items-center">
                     <div class="col-12 text-left text-light">
-                        <h1 class="font-weight-light">Our new website </h1>
-                        <p class="lead">is on its way</p>
+                        <h1 class="font-weight-light">Our new website is on its way</h1>
+                        <p class="lead">Check back soon for updates.</p>
+
                     </div>
                 </div>
             </div>
@@ -21,7 +22,7 @@
 
         <main role="main" class="container">
 
-            <p class="lead">Check back soon for updates.</p>
+            <p class="lead"></p>
         </main>
 
         <footer class="footer">
