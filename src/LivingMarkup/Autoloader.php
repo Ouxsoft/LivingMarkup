2cfd7de756d746992c68a8d24a1948c075945df9
hxtree <matthewheroux@gmail.com>
2020-05-29 18:46:14 -0400

    Create autoload class

--- src/LivingMarkup/Autoloader.php
+++ src/LivingMarkup/Autoloader.php
@@ -2,63 +2,79 @@
 /**
  * This file is part of the LivingMarkup package.
  *
- * (c) Matthew Heroux <matthewheroux@gmail.com>
+ * (c) 2017-2020 Ouxsoft  <contact@ouxsoft.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
-/**
- * Add a module to config
- * @param array $module
- * @return bool
- */
-function add_module(array $module): bool
-{
-    global $add_modules;
+namespace LivingMarkup;
 
-    if (!isset($add_modules)) {
-        $add_modules = [];
-    }
-    $add_modules[] = $module;
-    return true;
-}
+class Autoloader {
 
-/**
- * Callback to run kernel
- * @param string $buffer
- * @return string
- */
-function call_kernel(string $buffer) : string
-{
-    global $add_modules;
+    const PROCESS_TOGGLE = 'LHTML_OFF';
 
-    // return buffer if it's not HTML
-    if ($buffer==strip_tags($buffer)) {
-        return $buffer;
+    /**
+     * Autoloader constructor.
+     */
+    public function __construct(){
+        if (defined(self::PROCESS_TOGGLE )) {
+            return;
+        }
+
+        // process buffer once completed
+        ob_start([$this, 'process']);
     }
 
-    // instantiate Kernel
-    $kernel = new LivingMarkup\Kernel();
+    /**
+     * Add a module to config
+     * @param array $module
+     * @return bool
+     */
+    function addModule(array $module): bool
+    {
+        global $add_modules;
 
-    // instantiate Builder
-    $builder = new LivingMarkup\Builder\DynamicPageBuilder();
-
-    // load config
-    $config = new LivingMarkup\Configuration();
-
-    // add runtime modules to config
-    if (isset($add_modules)) {
-        $config->addModules($add_modules);
+        if (!isset($add_modules)) {
+            $add_modules = [];
+        }
+        $add_modules[] = $module;
+        return true;
     }
 
-    // add buffer to config
-    $config->add('markup', $buffer);
+    /**
+     * Callback to run kernel
+     * @param string $buffer
+     * @return string
+     */
+    function process(string $buffer) : string
+    {
+        global $add_modules;
 
-    // echo Kernel build of Builder
-    return $kernel->build($builder, $config);
-}
+        // return buffer if it's not HTML
+        if ($buffer==strip_tags($buffer)) {
+            return $buffer;
+        }
 
-if (!defined('LHTML_OFF')) {
-    ob_start('call_kernel');
+        // instantiate Kernel
+        $kernel = new LivingMarkup\Kernel();
+
+        // instantiate Builder
+        $builder = new LivingMarkup\Builder\DynamicPageBuilder();
+
+        // load config
+        $config = new LivingMarkup\Configuration();
+
+        // add runtime modules to config
+        if (isset($add_modules)) {
+            $config->addModules($add_modules);
+        }
+
+        // add buffer to config
+        $config->add('markup', $buffer);
+
+        // echo Kernel build of Builder
+        return $kernel->build($builder, $config);
+    }
+
 }
