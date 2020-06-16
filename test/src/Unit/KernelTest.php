8e2d9286401fcbb4778808a198fd81ace60542a2
hxtree <matthewheroux@gmail.com>
2020-06-15 21:55:19 -0400

    Add Kernel test

---
+++ test/src/Unit/KernelTest.php
@@ -0,0 +1,34 @@
+<?php
+/**
+* This file is part of the LivingMarkup package.
+*
+* (c) 2017-2020 Ouxsoft  <contact@ouxsoft.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace LivingMarkup\Test\LivingMarkup\Tests;
+
+use LivingMarkup\Builder\DynamicPageBuilder;
+use LivingMarkup\Configuration;
+use LivingMarkup\Kernel;
+use LivingMarkup\Engine;
+use PHPUnit\Framework\TestCase;
+
+class KernelTest extends TestCase
+{
+    public function testBuild()
+    {
+        $config_dir = dirname(__DIR__, 1) . '/inputs/phpunit.yml';
+        var_dump($config_dir);
+
+        $config = new Configuration($config_dir);
+        $config->add('markup', '<html><p>Hello, World!</p></html>');
+
+        $builder = new DynamicPageBuilder();
+        $new_page = (new Kernel())->build($builder, $config);
+
+        $this->assertInstanceOf(Engine::class, $new_page);
+    }
+}
