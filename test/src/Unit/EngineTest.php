ed985bf784fd19e5c13e7572d5d59e389fa9ee86
hxtree <matthewheroux@gmail.com>
2020-06-17 00:16:13 -0400

    Improve coverage

--- test/src/Unit/EngineTest.php
+++ test/src/Unit/EngineTest.php
@@ -16,6 +16,9 @@ use PHPUnit\Framework\TestCase;
 
 class EngineTest extends TestCase
 {
+    /**
+     * @covers \LivingMarkup\Engine::setType
+     */
     public function testSetType()
     {
         $config = new Configuration();
@@ -43,6 +46,9 @@ class EngineTest extends TestCase
         $this->assertIsArray($results);
     }
 
+    /**
+     * @covers \LivingMarkup\Engine::queryFetchAll
+     */
     public function testQueryFetchAll()
     {
         $config = new Configuration();
@@ -52,6 +58,9 @@ class EngineTest extends TestCase
         $this->assertCount(2, $results);
     }
 
+    /**
+     * @covers \LivingMarkup\Engine::getDomElementByPlaceholderId
+     */
     public function testGetDomElementByPlaceholderId()
     {
         $config = new Configuration();
@@ -62,6 +71,9 @@ class EngineTest extends TestCase
         $this->assertTrue($bool);
     }
 
+    /**
+     * @covers \LivingMarkup\Engine::getModuleInnerXML
+     */
     public function testGetModuleInnerXML()
     {
         $config = new Configuration();
@@ -72,7 +84,9 @@ class EngineTest extends TestCase
         $this->assertTrue($bool);
     }
 
-
+    /**
+     * @covers \LivingMarkup\Engine::getElementArgs
+     */
     public function testGetElementArgs()
     {
         $config = new Configuration();
@@ -84,7 +98,9 @@ class EngineTest extends TestCase
         $this->assertTrue($bool);
     }
 
-
+    /**
+     * @covers \LivingMarkup\Engine::__toString
+     */
     public function test__toString()
     {
         $test_results = '<!DOCTYPE html>
@@ -97,6 +113,9 @@ class EngineTest extends TestCase
         $this->assertTrue($bool);
     }
 
+    /**
+     * @covers \LivingMarkup\Engine::queryFetch
+     */
     public function testQueryFetch()
     {
         $config = new Configuration();
@@ -107,6 +126,9 @@ class EngineTest extends TestCase
         $this->assertTrue($bool);
     }
 
+    /**
+     * @covers \LivingMarkup\Engine::instantiateModules
+     */
     public function testInstantiateModules()
     {
         $config = new Configuration();
@@ -121,6 +143,9 @@ class EngineTest extends TestCase
         $this->assertCount(1, $engine->module_pool);
     }
 
+    /**
+     * @covers \LivingMarkup\Engine::callHook
+     */
     public function testCallHook()
     {
         $config = new Configuration();
@@ -140,21 +165,50 @@ class EngineTest extends TestCase
     }
 
 
-    /*
-     * // TODO: add following
-
+    /**
+     * @covers \LivingMarkup\Engine::replaceDomElement
+     */
     public function testReplaceDomElement()
     {
-
+        $config = new Configuration();
+        $config->setSource('<html><b '.Engine::INDEX_ATTRIBUTE.'="test"><arg name="toggle">no</arg></b></html>');
+        $engine = new Engine($config);
+        $dom_element = $engine->getDomElementByPlaceholderId('test');
+        $engine->replaceDomElement($dom_element, '<b>Foo Bar</b>');
+        $this->assertStringContainsString($engine, '<!DOCTYPE html>
+<html><b>Foo Bar</b></html>
+');
     }
 
+    /**
+     * @covers \LivingMarkup\Engine::renderModule
+     */
     public function testRenderModule()
     {
+        $config = new Configuration();
+        $config->setSource('<html><div>Foo Bar</div></html>');
+        $engine = new Engine($config);
+        $engine->instantiateModules(
+            [
+                'xpath' => '//div',
+                'class_name' => 'LivingMarkup\Test\HelloWorld'
+            ]
+        );
+        foreach($engine->module_pool as $module){
 
+            $engine->renderModule($module->module_id);
+
+        }
+
+        $this->assertStringContainsString($engine, '<!DOCTYPE html>
+<html>Hello, World</html>
+');
     }
-    */
 
-    // private class cannot test directly, instead we're using InstantiateModules
+    /**
+     * private class cannot test directly, instead we're using InstantiateModules
+     * @covers \LivingMarkup\Engine::instantiateModule
+     */
     public function testInstantiateModule()
     {
         $config = new Configuration();
@@ -169,6 +223,9 @@ class EngineTest extends TestCase
         $this->assertCount(2, $engine->module_pool);
     }
 
+    /**
+     * @covers \LivingMarkup\Engine::getModuleAncestorProperties
+     */
     public function testGetModuleAncestorProperties()
     {
         $config = new Configuration();
@@ -186,6 +243,9 @@ class EngineTest extends TestCase
         }
     }
 
+    /**
+     * @covers \LivingMarkup\Engine::__construct
+     */
     public function test__construct()
     {
         $config = new Configuration();
