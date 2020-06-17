06aba6ae41a62a6bb594007af5fb962945008521
hxtree <matthewheroux@gmail.com>
2020-06-16 20:40:46 -0400

    Switch back to Lamanis YAml parser and add coverals file

--- src/LivingMarkup/Configuration.php
+++ src/LivingMarkup/Configuration.php
@@ -14,7 +14,7 @@ declare(strict_types=1);
 namespace LivingMarkup;
 
 use LivingMarkup\Exception\Exception;
-use Symfony\Component\Yaml\Yaml;
+use Laminas\Config\Reader\Yaml;
 use Laminas\Validator\File\Exists;
 
 /**
@@ -64,8 +64,8 @@ class Configuration
                 }
 
                 // load yaml file
-
-                $this->config =  Yaml::parseFile($this->path);
+                $reader = new Yaml();
+                $this->config = $reader->fromFile($this->path);
 
                 if (empty($this->config)) {
                     return false;
