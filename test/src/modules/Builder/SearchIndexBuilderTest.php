0b809c6717dc6ef83540c360521c63307f212072
hxtree <matthewheroux@gmail.com>
2020-06-20 18:58:17 -0400

    Placeholder search engine builder

---
+++ test/src/modules/Builder/SearchIndexBuilderTest.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * This file is part of the LivingMarkup package.
+ *
+ * (c) 2017-2020 Ouxsoft  <contact@ouxsoft.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace LivingMarkup\Test\Builder;
+
+use LivingMarkup\Builder\SearchIndexBuilder;
+use LivingMarkup\Processor;
+use PHPUnit\Framework\TestCase;
+
+class SearchIndexBuilderTest extends TestCase
+{
+
+    /**
+     * @covers \LivingMarkup\Builder\SearchIndexBuilder::getObject
+     * @codeCoverageIgnore
+     */
+    public function testGetObject()
+    {
+
+    }
+
+    /**
+     * @covers \LivingMarkup\Builder\SearchIndexBuilder::getObject
+     * @codeCoverageIgnore
+     */
+    public function testCreateObject()
+    {
+
+    }
+}
