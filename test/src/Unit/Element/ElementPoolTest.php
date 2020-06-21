f7f541a8d540f9d0b71762f70e308a2ddd35d021
hxtree <matthewheroux@gmail.com>
2020-06-21 00:07:39 -0400

    Increase test coverage

--- test/src/Unit/Element/ElementPoolTest.php
+++ test/src/Unit/Element/ElementPoolTest.php
@@ -7,63 +7,79 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-/*
+
 namespace LivingMarkup\Test\Element;
 
+use LivingMarkup\Element\AbstractElement;
 use LivingMarkup\Element\ElementPool;
+use LivingMarkup\Test\HelloWorld;
 use PHPUnit\Framework\TestCase;
 
-/*
 class ElementPoolTest extends TestCase
 {
 
     /**
      * @covers \LivingMarkup\Element\ElementPool::getIterator
      */
-    /*
     public function testGetIterator()
     {
-
+        $pool = new ElementPool;
+        $this->assertTrue(($pool->getIterator() instanceof \ArrayIterator));
     }
 
     /**
      * @covers \LivingMarkup\Element\ElementPool::getPropertiesById
      */
-  /*  public function testGetPropertiesById()
+    public function testGetPropertiesById()
     {
-
+        $pool = new ElementPool;
+        $lhtml_element = new HelloWorld();
+        $pool->add($lhtml_element);
+        $this->assertIsArray($pool->getPropertiesById($lhtml_element->element_id));
     }
 
     /**
      * @covers \LivingMarkup\Element\ElementPool::getById
      */
-    /*public function testGetById()
+    public function testGetById()
     {
+        $pool = new ElementPool;
+        $lhtml_element = new HelloWorld();
+        $pool->add($lhtml_element);
+        $this->assertTrue(($pool->getById($lhtml_element->element_id) instanceof AbstractElement));
 
+        $this->assertFalse(($pool->getById('missing') instanceof AbstractElement));
     }
 
     /**
      * @covers \LivingMarkup\Element\ElementPool::add
      */
-    /*public function testAdd()
+    public function testAdd()
     {
-
+        $pool = new ElementPool;
+        $lhtml_element = new HelloWorld();
+        $pool->add($lhtml_element);
+        $this->assertTrue(($pool->getById($lhtml_element->element_id) instanceof AbstractElement));
     }
 
     /**
      * @covers \LivingMarkup\Element\ElementPool::callMethod
      */
-    /*public function testCallMethod()
+    public function testCallMethod()
     {
-
+        $pool = new ElementPool();
+        $results = $pool->callMethod('onRender');
+        $this->assertNull($results);
     }
 
     /**
      * @covers \LivingMarkup\Element\ElementPool::count
      */
-    /*public function testCount()
+    public function testCount()
     {
-
+        $pool = new ElementPool;
+        $lhtml_element = new HelloWorld();
+        $pool->add($lhtml_element);
+        $this->assertCount(1,$pool);
     }
 }
-*/
