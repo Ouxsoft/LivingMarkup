0f9094dbe46f9e1b257594716cc8273213e8c911
hxtree <matthewheroux@gmail.com>
2021-02-11 21:30:48 -0600

    Update copyright year

--- src/Contract/ConfigurationInterface.php
+++ src/Contract/ConfigurationInterface.php
@@ -1,8 +1,41 @@
 <?php
+/*
+ * This file is part of the LivingMarkup package.
+ *
+ * (c) 2017-2021 Ouxsoft  <contact@ouxsoft.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Ouxsoft\LivingMarkup\Contract;
 
 interface ConfigurationInterface
 {
+    public function __construct(
+        DocumentInterface &$document,
+        ?string $config_file_path = null
+    );
 
+    public function loadFile(string $filepath = null): void;
+
+    public function clearConfig(): void;
+
+    public function setConfig(array $config): void;
+
+    public function setMarkup(string $markup): void;
+
+    public function addElement(array $element): void;
+
+    public function addElements(array $elements): void;
+
+    public function getElements(): array;
+
+    public function addRoutine(array $routine): void;
+
+    public function addRoutines(array $routines): void;
+
+    public function getRoutines(): array;
+
+    public function getMarkup(): string;
 }
--- src/Factory/ContainerFactory.php
+++ src/Factory/ContainerFactory.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of the LivingMarkup package.
+ *
+ * (c) 2017-2021 Ouxsoft  <contact@ouxsoft.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 declare(strict_types=1);
 
